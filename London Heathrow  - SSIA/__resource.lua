3dec7f2f8f79c84f03a1dd9a5467d1cabed1ef35
Cody <67431148+Codster26@users.noreply.github.com>
2020-07-30 18:03:51 -0400

    Delete __resource.lua

--- London Heathrow  - SSIA/__resource.lua
+++
@@ -1,9 +0,0 @@
--- London Heathrow (SSIA)
--- Map by Codster: https://www.gta5-mods.com/maps/procopio-international-airport
-
-description 'London Heathrow (SSIA) - SSIA Replacement'
-
-resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
-this_is_a_map 'yes'
-
-data_file 'DLC_ITYP_REQUEST' 'stream/props.ytyp'
